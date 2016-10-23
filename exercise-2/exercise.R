1d6727cbff425d31cea939256156135846c30999
Vinny <vinayakbansal@hotmail.com>
2016-10-23 16:20:27 -0700

    Done with 2 and 7

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,30 +3,35 @@
 
 # Install devtools package: allows installations from GitHub
 install.packages('devtools')
-
+install.packages('dplyr')
 # Install "fueleconomy" package from GitHub
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
-
+library(fueleconomy)
+library(dplyr)
 # You should have have access to the `vehicles` data.frame
-
+is.data.frame(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+vehicles.1997.2 <- filter(vehicles, year == 1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(vehicles.1997.2$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+vehicles.2.20 <- filter(vehicles, drive == "2-Wheel Drive" & cty > 20)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+worst.hwy <- select(filter(vehicles.2.20, hwy == min(hwy)), id)
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
+hwympg <- function(year, make){
+  filtered <- filter(vehicles, make == make, year == year) %>% 
+    filter(hwy == max(hwy))
+  return(filtered)
+}
 
 
 # What was the most efficient honda model of 1995?
--- exercise-7/exercise.r
+++ exercise-7/exercise.r
@@ -1,33 +1,43 @@
 # Exercise 7: DPLYR practice with NBA data
-
+install.packages("dplyr")
+library(dplyr)
 # Read in the NBA player data into a variable called `team.data` using `read.csv`
-
+team.data <- read.csv('data/teams.csv')
 # The data.frame team.data should now be accessible to you. 
 # View it, and get some basic information about the number of rows/columns. 
 # Note the "X" preceding some of the column titles as well as the "*" 
 # following the names of teams that made it to the playoffs that year.
-
-
+nrow(team.data)
+ncol(team.data)
 # Add a column that gives the turnovers to steals ratio (TOV / STL) for each team
-
+team.data <- mutate(team.data, turnover = TOV / STL)
 
 # Sort the teams from lowest turnover/steal ratio to highest
+team.data.turnover <- arrange(team.data, turnover)
 # Which team has the lowest ratio?
-
+lowest.team.data <- filter(team.data, turnover == min(turnover))%>%
+  select(Team)
 # Using the pipe operator, create a new column of assists per game (AST / G) AND
 # sort the data.frame by this new column in DESCENDING order.
-              
+team.data.assists <- mutate(team.data, assistspergame = AST / G) %>% arrange(-assistspergame)
 # Create a data.frame called `good.offense` of teams that scored more than 8700 points (PTS)
-
+good.offense <- filter(team.data, PTS > 8700)
 # Create a data.frame called `good.defense` of teams that had more than 470 blocks (BLK)
-
+good.defense <- filter(team.data, BLK > 470)
 # Create a data.frame called `defense.stats` that only shows defensive rebounds (DRB), steals (STL),
 # and blocks (BLK) along with the team name.
-
+defense.stats <- select(team.data, BLK, Team, DRB, STL)
 # Create a data.frame called `offense.stats` that only shows offensive rebounds (ORB), 
 # field-goal % (FG.), and assists (AST) along with the team name.
-
+offense.stats <- select(team.data, ORB, AST, FG., Team)
 ### Bonus ###
 # Create a function called `BetterShooters` that takes in two teams and returns
 # a data.frame of the team with the better field-goal percentage. The returned 
 # data.frame should include the team name, field-goal percentage, and points.
+BetterShooters <- function(Team1, Team2){
+  filter(team.data, Team %in% c(Team1, Team2)) %>% 
+    filter(FG. == max(FG.)) %>%
+    select(Team, FG., PTS)
+  
+  return (better.team)
+}
